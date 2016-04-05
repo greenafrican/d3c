538a361ad87ecf73686a93d98fab8475bf167cd1
Robert Elliott <robert@graphflow.com>
2016-04-05 08:48:03 +0200

    simple table headers

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps');
+
+gulp.task("concatScripts", function() {
+    return gulp.src([
+            'src/js/moment.js',
+            'src/js/calendar.js',
+            'src/js/core.js'
+        ])
+        .pipe(maps.init())
+        .pipe(concat('d3c.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('src/js'));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+    return gulp.src("src/js/d3c.js")
+        .pipe(uglify())
+        .pipe(rename('d3c.min.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compileSass', function() {
+    return gulp.src("scss/application.scss")
+        .pipe(maps.init())
+        .pipe(sass())
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watchSass', function() {
+    gulp.watch('src/scss/**/*.scss', ['compileSass']);
+});
+
+gulp.task("build", ['minifyScripts', 'compileSass']);
+
+gulp.task("default", ["build"]);
+
+
+
---
+++ src/js/core.js
@@ -0,0 +1,56 @@
+$( function() {
+
+    //var d3c = { version: "0.0.1" };
+
+    function Table(config) {
+        this.bindto = (bindto in config) ? config.bindto : "#d3c-table";
+        this.columns = (columns in config) ? config.columns : [];
+        this.data = (data in config) ? config.data : [];
+    }
+
+    Table.prototype.add = function(data) {
+        this.data.push(data);
+    };
+
+    Table.prototype.redraw = function() {
+        this.selectTable = d3.select(this.bindto)
+            .append('table')
+            .append('thead')
+            .append('tr')
+            .selectAll('th')
+            .data(this.columns).enter()
+            .append('th')
+            .style('width', function(d, i) {return d.width;})
+            .attr('class', function(d, i) {return 'd3c-th';})
+            .text(function(d) {return d.title;});
+    };
+
+    var d3c = new Table({
+        bindto: "#d3c-table",
+        columns: [{
+                title: "Name",
+                width: "15%"
+            },
+            {
+                title: "Latest",
+                width: "15%"
+            },
+            {
+                title: "Previous",
+                width: "15%"
+            },
+            {
+                title: "Change",
+                width: "15%"
+            },
+            {
+                title: "Change",
+                width: "40%"
+            }]
+    });
+
+    d3c.redraw();
+
+});
+
+
