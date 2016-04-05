9612bef648b6d1bd856cc2b8f8231bdbf1c9d4fd
Robert Elliott <robert@graphflow.com>
2016-04-05 09:33:22 +0200

    simple addRow

--- dist/d3c.js
+++ dist/d3c.js
@@ -12236,21 +12236,68 @@ $( function() {
         this.data = ('data' in config) ? config.data : [];
     }
 
-    Table.prototype.add = function(data) {
-        this.data.push(data);
+    Table.prototype.addRow = function(row) {
+
+        var count = 0;
+        for (var k in row) {
+            if (row.hasOwnProperty(k)) {
+                ++count;
+            }
+        }
+        if (count !== this.columns.length) {
+            alert("Row data structure doesn't match column definition"); // TODO: handle mismatch
+            return;
+        }
+
+        this.data.push(row);
+        this.redraw();
+
     };
 
     Table.prototype.redraw = function() {
-        this.selectTable = d3.select(this.bindto)
-            .append('table')
-            .append('thead')
-            .append('tr')
-            .selectAll('th')
-            .data(this.columns).enter()
-            .append('th')
-            .style('width', function(d, i) {return d.width;})
-            .attr('class', function(d, i) {return 'd3c-th';})
-            .text(function(d) {return d.title;});
+        var data = this.data;
+
+        if (data.length > 0) {
+
+            this.selectTable = d3.select(this.bindto)
+                .append('table');
+
+            this.selectTable
+                .append('thead')
+                .append('tr')
+                .selectAll('th')
+                .data(this.columns).enter()
+                .append('th')
+                .style('width', function (d, i) {
+                    return d.width;
+                })
+                .attr('class', function (d) {
+                    return 'd3c-th';
+                })
+                .text(function (d) {
+                    return d.title;
+                });
+
+            this.width = d3.select(this.bindto + " table").node().getBoundingClientRect().width;
+
+            this.selectTable.append('tbody')
+                .selectAll('tr')
+                .data(data).enter()
+                .append('tr')
+                .selectAll('td')
+                .data(function (d) {
+                    return d3.values(d);
+                })
+                .enter().append('td')
+                .attr('class', function (d, i) {
+                    return 'd3c-td';
+                })
+                .text(function (d, i) {
+                    return d;
+                });
+        } else {
+            alert("Table has no data!"); // TODO: gracefully handle no data
+        }
     };
 
     var d3c = new Table({
@@ -12277,7 +12324,13 @@ $( function() {
             }]
     });
 
-    d3c.redraw();
+    d3c.addRow({
+        'name': 'Active Users',
+        'latest': 2500.00,
+        'previous': 14865.00,
+        'per_change': 0.0375,
+        'chart_change': 0.0375
+    });
 
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
@@ -7,7 +7,7 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     maps = require('gulp-sourcemaps');
 
-gulp.task("concatScripts", function() {
+gulp.task('concatScripts', function() {
     return gulp.src([
             'src/js/moment.js',
             'src/js/calendar.js',
@@ -20,15 +20,15 @@ gulp.task("concatScripts", function() {
         .pipe(gulp.dest('src/js'));
 });
 
-gulp.task("minifyScripts", ["concatScripts"], function() {
-    return gulp.src("src/js/d3c.js")
+gulp.task('minifyScripts', ['concatScripts'], function() {
+    return gulp.src('src/js/d3c.js')
         .pipe(uglify())
         .pipe(rename('d3c.min.js'))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('compileSass', function() {
-    return gulp.src("scss/application.scss")
+    return gulp.src('scss/application.scss')
         .pipe(maps.init())
         .pipe(sass())
         .pipe(maps.write('./'))
@@ -39,9 +39,13 @@ gulp.task('watchSass', function() {
     gulp.watch('src/scss/**/*.scss', ['compileSass']);
 });
 
-gulp.task("build", ['minifyScripts', 'compileSass']);
+gulp.task('watchJs', function() {
+    gulp.watch('src/js/**/*.js', ['minifyScripts']);
+});
 
-gulp.task("default", ["build"]);
+gulp.task('build', ['minifyScripts', 'compileSass']);
+
+gulp.task('default', ['build', 'watchJs']);
 
 
 
