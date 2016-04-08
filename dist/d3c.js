dbdc843fb5691fc5e623f30480d823f636c9420b
Robert Elliott <robert@graphflow.com>
2016-04-08 11:35:25 +0200

    font color picker based on bg

--- dist/d3c.js
+++ dist/d3c.js
@@ -321,7 +321,8 @@ function drawCell(selection) {
                 .style('text-align', 'center')
                 .text(function(d) {
                     return formatText(d);
-                });
+                })
+                .style('color', function(d) { return pickColor(this);});
         } else {
             $$.text(function(d) {
                 return formatText(d);
@@ -371,6 +372,22 @@ function formatText(d) {
         default:
             return d.value;
     }
+}
+
+function pickColor(selection) {
+    var d = d3.select(selection);
+    var c = d3.values(d3.rgb(d.style('background-color'))).slice(0, 3);
+    for ( var i = 0; i < c.length; ++i ) {
+        c[i] = c[i] / 255;
+        if ( c[i] <= 0.03928 ) {
+            c[i] = c[i] / 12.92
+        } else {
+            c[i] = Math.pow( ( c[i] + 0.055 ) / 1.055, 2.4);
+        }
+    }
+    var l = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
+    //return ( l > 0.179 ) ? 'black' :'white';
+    return ( l > 0.5 ) ? 'black' :'white';
 }
 
 d3c.table = function (config) {
