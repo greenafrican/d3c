d2e01e2b5ae24f7795be2a53c2ccd2c0e07b2bad
Robert Elliott <robert@graphflow.com>
2016-04-13 10:00:39 +0200

    rowSelect allow config

--- dist/d3c.js
+++ dist/d3c.js
@@ -38,7 +38,7 @@ function pickColor(color) {
 }
 
 
-Table.prototype.chart = function(config) {
+Table.prototype.chart = function (config) {
     if (arguments.length === 0 || $.isEmptyObject(config)) return this._chart || {};
     config || (config = {});
     this.chartConfig(config);
@@ -46,21 +46,40 @@ Table.prototype.chart = function(config) {
     return this._chart;
 };
 
-Table.prototype.chartConfig = function(config) {
+Table.prototype.chartConfig = function (config) {
     if (arguments.length === 0 || $.isEmptyObject(config)) return this._chart_config || {};
     config || (config = {});
     this._chart_config = config;
     return this._chart_config; // TODO: update if config changes and exists
 };
 
-Table.prototype.rowSelect = function(row) {
+Table.prototype.rowSelect = function (row) {
     var self = this;
-    row.forEach(function(cell, i) {
+    row.forEach(function (cell, i) {
         if (cell.key === 'series') {
             console.log(cell.value);
             self.chart().load(
                 {
-                    json: cell.value
+                    json: cell.value,
+                    names: {
+                        'previous': 'Previous',
+                        'latest': 'Latest'
+                    },
+                    keys: {
+                        x: 'date',
+                        value: ['latest', 'previous']
+                    },
+                    type: 'line',
+                    types: {
+                        'previous': 'area'
+                    },
+                    classes: {
+                        'previous': 'd3c-previous'
+                    },
+                    colors: {
+                        'previous': '#e5e5e5',
+                        'latest': '#64bc52'
+                    }
                 }
             );
         }
