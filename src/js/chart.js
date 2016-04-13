2c6e81ad85b466eba74d63b2461232a98eccf81b
Robert Elliott <robert@graphflow.com>
2016-04-13 10:10:23 +0200

    rowSelect allow config

--- src/js/chart.js
+++ src/js/chart.js
@@ -17,29 +17,10 @@ Table.prototype.rowSelect = function (row) {
     var self = this;
     row.forEach(function (cell, i) {
         if (cell.key === 'series') {
-            console.log(cell.value);
+            console.log(cell.value.toString());
             self.chart().load(
                 {
-                    json: cell.value,
-                    names: {
-                        'previous': 'Previous',
-                        'latest': 'Latest'
-                    },
-                    keys: {
-                        x: 'date',
-                        value: ['latest', 'previous']
-                    },
-                    type: 'line',
-                    types: {
-                        'previous': 'area'
-                    },
-                    classes: {
-                        'previous': 'd3c-previous'
-                    },
-                    colors: {
-                        'previous': '#e5e5e5',
-                        'latest': '#64bc52'
-                    }
+                    json: cell.value
                 }
             );
         }
